f48504e7753840e332fcf3609e7d6af0e62519e7
Radu Cordunianu <cordunianuradu@gmail.com>
2022-07-05 22:55:35 +0300

    commit

---
+++ Blackjack/Blackjack/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Blackjack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labeltest = new System.Windows.Forms.Label();
+            this.buttonhit = new System.Windows.Forms.Button();
+            this.labelscor = new System.Windows.Forms.Label();
+            this.labeldealer = new System.Windows.Forms.Label();
+            this.labelscordealer = new System.Windows.Forms.Label();
+            this.buttonstand = new System.Windows.Forms.Button();
+            this.buttondoubledown = new System.Windows.Forms.Button();
+            this.buttonrestart = new System.Windows.Forms.Button();
+            this.labelcredite = new System.Windows.Forms.Label();
+            this.labelmiza = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonsurrender = new System.Windows.Forms.Button();
+            this.insurance = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labeltest
+            // 
+            this.labeltest.AutoSize = true;
+            this.labeltest.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labeltest.Location = new System.Drawing.Point(153, 79);
+            this.labeltest.Name = "labeltest";
+            this.labeltest.Size = new System.Drawing.Size(173, 41);
+            this.labeltest.TabIndex = 0;
+            this.labeltest.Text = "carti jucator";
+            // 
+            // buttonhit
+            // 
+            this.buttonhit.Location = new System.Drawing.Point(153, 473);
+            this.buttonhit.Name = "buttonhit";
+            this.buttonhit.Size = new System.Drawing.Size(113, 40);
+            this.buttonhit.TabIndex = 1;
+            this.buttonhit.Text = "Hit";
+            this.buttonhit.UseVisualStyleBackColor = true;
+            this.buttonhit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelscor
+            // 
+            this.labelscor.AutoSize = true;
+            this.labelscor.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelscor.Location = new System.Drawing.Point(153, 307);
+            this.labelscor.Name = "labelscor";
+            this.labelscor.Size = new System.Drawing.Size(252, 41);
+            this.labelscor.TabIndex = 2;
+            this.labelscor.Text = "suma carti jucator";
+            // 
+            // labeldealer
+            // 
+            this.labeldealer.AutoSize = true;
+            this.labeldealer.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labeldealer.Location = new System.Drawing.Point(614, 79);
+            this.labeldealer.Name = "labeldealer";
+            this.labeldealer.Size = new System.Drawing.Size(164, 41);
+            this.labeldealer.TabIndex = 3;
+            this.labeldealer.Text = "carti dealer";
+            // 
+            // labelscordealer
+            // 
+            this.labelscordealer.AutoSize = true;
+            this.labelscordealer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelscordealer.Location = new System.Drawing.Point(614, 307);
+            this.labelscordealer.Name = "labelscordealer";
+            this.labelscordealer.Size = new System.Drawing.Size(240, 36);
+            this.labelscordealer.TabIndex = 4;
+            this.labelscordealer.Text = "suma carti dealer";
+            // 
+            // buttonstand
+            // 
+            this.buttonstand.Location = new System.Drawing.Point(328, 473);
+            this.buttonstand.Name = "buttonstand";
+            this.buttonstand.Size = new System.Drawing.Size(113, 40);
+            this.buttonstand.TabIndex = 5;
+            this.buttonstand.Text = "Stand";
+            this.buttonstand.UseVisualStyleBackColor = true;
+            this.buttonstand.Click += new System.EventHandler(this.buttonstand_Click);
+            // 
+            // buttondoubledown
+            // 
+            this.buttondoubledown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttondoubledown.Location = new System.Drawing.Point(490, 473);
+            this.buttondoubledown.Name = "buttondoubledown";
+            this.buttondoubledown.Size = new System.Drawing.Size(167, 40);
+            this.buttondoubledown.TabIndex = 6;
+            this.buttondoubledown.Text = "Double Down";
+            this.buttondoubledown.UseVisualStyleBackColor = true;
+            this.buttondoubledown.Click += new System.EventHandler(this.buttondoubledown_Click);
+            // 
+            // buttonrestart
+            // 
+            this.buttonrestart.Location = new System.Drawing.Point(445, 568);
+            this.buttonrestart.Name = "buttonrestart";
+            this.buttonrestart.Size = new System.Drawing.Size(181, 36);
+            this.buttonrestart.TabIndex = 7;
+            this.buttonrestart.Text = "Joc nou";
+            this.buttonrestart.UseVisualStyleBackColor = true;
+            this.buttonrestart.Click += new System.EventHandler(this.buttonrestart_Click);
+            // 
+            // labelcredite
+            // 
+            this.labelcredite.AutoSize = true;
+            this.labelcredite.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelcredite.Location = new System.Drawing.Point(85, 559);
+            this.labelcredite.Name = "labelcredite";
+            this.labelcredite.Size = new System.Drawing.Size(209, 41);
+            this.labelcredite.TabIndex = 8;
+            this.labelcredite.Text = "Credite: 10000";
+            this.labelcredite.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelmiza
+            // 
+            this.labelmiza.AutoEllipsis = true;
+            this.labelmiza.AutoSize = true;
+            this.labelmiza.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelmiza.Location = new System.Drawing.Point(708, 563);
+            this.labelmiza.Name = "labelmiza";
+            this.labelmiza.Size = new System.Drawing.Size(96, 41);
+            this.labelmiza.TabIndex = 9;
+            this.labelmiza.Text = "Miza: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(810, 561);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(131, 47);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // buttonsurrender
+            // 
+            this.buttonsurrender.Location = new System.Drawing.Point(708, 473);
+            this.buttonsurrender.Name = "buttonsurrender";
+            this.buttonsurrender.Size = new System.Drawing.Size(182, 40);
+            this.buttonsurrender.TabIndex = 11;
+            this.buttonsurrender.Text = "Surrender";
+            this.buttonsurrender.UseVisualStyleBackColor = true;
+            this.buttonsurrender.Click += new System.EventHandler(this.buttonsurrender_Click);
+            // 
+            // insurance
+            // 
+            this.insurance.AutoSize = true;
+            this.insurance.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.insurance.Location = new System.Drawing.Point(614, 79);
+            this.insurance.Name = "insurance";
+            this.insurance.Size = new System.Drawing.Size(0, 41);
+            this.insurance.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 680);
+            this.Controls.Add(this.insurance);
+            this.Controls.Add(this.buttonsurrender);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelmiza);
+            this.Controls.Add(this.labelcredite);
+            this.Controls.Add(this.buttonrestart);
+            this.Controls.Add(this.buttondoubledown);
+            this.Controls.Add(this.buttonstand);
+            this.Controls.Add(this.labelscordealer);
+            this.Controls.Add(this.labeldealer);
+            this.Controls.Add(this.labelscor);
+            this.Controls.Add(this.buttonhit);
+            this.Controls.Add(this.labeltest);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blackjack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labeltest;
+        private System.Windows.Forms.Button buttonhit;
+        private System.Windows.Forms.Label labelscor;
+        private System.Windows.Forms.Label labeldealer;
+        private System.Windows.Forms.Label labelscordealer;
+        private System.Windows.Forms.Button buttonstand;
+        private System.Windows.Forms.Button buttondoubledown;
+        private System.Windows.Forms.Button buttonrestart;
+        private System.Windows.Forms.Label labelcredite;
+        private System.Windows.Forms.Label labelmiza;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonsurrender;
+        private System.Windows.Forms.Label insurance;
+    }
+}
+
---
+++ Blackjack/Blackjack/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blackjack
+{
+    public partial class Form1 : Form
+    {
+        private bool Gasit, ContineAs, ContineAsDealer, EsteInsurance;
+        private int cnt, CartiTrase, CartiDealer, credite, miza;
+        private int[] carti;
+        private string[] numecarti;
+        private string tipDeCarte;
+        System.Media.SoundPlayer norocos = new System.Media.SoundPlayer();
+        public Form1()
+        {
+            carti = new int[53];
+            norocos.SoundLocation = "noroc.wav";
+            numecarti = new string[53];
+            CartiTrase = CartiDealer = 0;
+            credite = 10000;
+            ContineAs = false; ContineAsDealer = false; 
+            InitializeComponent();
+            cnt = 0;
+            labeltest.Text = ""; labeldealer.Text = ""; labelscor.Text = ""; labelscordealer.Text = "";
+            GenerareCarti();
+            buttonhit.Visible = false; buttonstand.Visible = false; buttondoubledown.Visible = false; buttonsurrender.Visible = false;
+            insurance.Font = new Font("Segoe UI", 16, FontStyle.Regular);
+        }
+        //genereaza cartile in ordinea A♠,2♠,...,A♣,2♣,...,A♥,2♥,...,A♦,2♦
+        private void GenerareCarti()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0) tipDeCarte = "♠";
+                else if (i == 1) tipDeCarte = "♣";
+                else if (i == 2) tipDeCarte = "♥";
+                else tipDeCarte = "♦";
+                for (int j = 0; j < 13; j++)
+                {
+                    carti[i] = (i + 1) * (j + 1);
+                    if (j == 0)
+                        numecarti[cnt++] = "A" + tipDeCarte;
+                    else if (j < 10)
+                        numecarti[cnt++] = Convert.ToString(j + 1)  + tipDeCarte;
+                    else if (j == 10)
+                        numecarti[cnt++] = "J " + tipDeCarte;
+                    else if (j == 11)
+                        numecarti[cnt++] = "Q " + tipDeCarte;
+                    else if (j == 12)
+                        numecarti[cnt++] = "K" + tipDeCarte;
+                }
+            } 
+        }
+
+        //Stand.
+        private void buttonstand_Click(object sender, EventArgs e)
+        {
+            while (CartiDealer < 17)
+                HitDealer();
+            if(EsteInsurance==true)
+            {
+                labeldealer.Visible = true;
+                labelscordealer.Visible = true;
+                insurance.Text = "";
+            }
+            if (CartiTrase > CartiDealer || CartiDealer>21)
+            {
+                MessageBox.Show("Ai castigat!");
+                credite += 2 * miza;
+            }
+            else if (CartiTrase == CartiDealer)
+            {
+                MessageBox.Show("Push");
+                credite += miza;
+            }
+            else
+            {
+                MessageBox.Show("Ai pierdut");
+                if(credite==0)
+                {
+                    MessageBox.Show("Ai ramas fara credite. Jocul se va inchide.");
+                    Application.Exit();
+                }    
+            }
+            labelcredite.Text = "Credite: " + Convert.ToString(credite);
+            JocTerminat();
+
+        }
+
+        //double down. Jucatorul dubleaza pariul initial, trage o singura carte si se opreste automat
+        private void DoubleDown()
+        {
+            Hit();
+            if (CartiTrase > 21)
+            {
+                MessageBox.Show("Ai pierdut");
+                if (credite <=10)
+                {
+                    MessageBox.Show("Ai ramas fara credite. Jocul se va inchide.");
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                if (EsteInsurance == true)
+                {
+                    labeldealer.Visible = true;
+                    labelscordealer.Visible = true;
+                    insurance.Text = "";
+                }
+                while (CartiDealer < 17)
+                    HitDealer();
+                if (CartiTrase > CartiDealer || CartiDealer > 21)
+                {
+                    MessageBox.Show("Ai castigat!");
+                    credite += 4 * miza; //pariu initial + dublu
+                }
+                else if (CartiTrase == CartiDealer)
+                {
+                    MessageBox.Show("Push");
+                    credite += 2 * miza;
+                }
+                else
+                {
+                    MessageBox.Show("Ai pierdut");
+                    if (credite <=10)
+                    {
+                        MessageBox.Show("Ai ramas fara credite. Jocul se va inchide.");
+                        Application.Exit();
+                    }
+                }
+            }
+            labelcredite.Text = "Credite: " + Convert.ToString(credite);
+            JocTerminat();
+        }
+
+        private void buttondoubledown_Click(object sender, EventArgs e)
+        {
+            if (credite < miza)
+                MessageBox.Show("Nu ai suficiente credite");
+            else
+            {
+                credite -= miza;
+                DoubleDown();
+            }
+        }
+
+        //reseteaza jocul
+        private void ReseteazaJoc()
+        {
+            numericUpDown1.Maximum = credite;
+            miza = (int)numericUpDown1.Value;
+            EsteInsurance = false;
+            carti = new int[53];
+            numecarti = new string[53];
+            credite -= miza;
+            labelcredite.Text = "Credite: " + Convert.ToString(credite);
+            CartiTrase = CartiDealer = 0;
+            ContineAs = false; ContineAsDealer = false;
+            cnt = 0;
+            labeltest.Text = ""; labeldealer.Text = "";
+            GenerareCarti();
+            ///labeltest.Text = numecarti[51];
+            Hit();
+            Hit();
+            HitDealer();
+            if (CartiTrase == 21)
+            {
+                MessageBox.Show("Blackjack!");
+                credite += (3 * miza) / 2;
+                norocos.Play();
+                JocTerminat();
+            }
+
+            //verifica daca dealerul are as in mana
+            if (CartiDealer == 11)
+                if (MessageBox.Show("Dealerul a tras un as, doresti sa te asiguri?", "Insurance", MessageBoxButtons.YesNo)
+                    == DialogResult.Yes)
+                {
+                    EsteInsurance = true;
+                    credite -= (int)miza / 2;
+                    insurance.Text = labeldealer.Text;
+                    labeldealer.Visible = false;
+                    labelscordealer.Visible = false;
+                    HitDealer();
+                    if (CartiDealer == 21)
+                    {
+                        MessageBox.Show("Dealerul a avut blackjack!");
+                        credite += miza + (int)miza / 2;
+                        labeldealer.Visible = true;
+                        labelscordealer.Visible = true;
+                        JocTerminat();
+                    }
+                    else MessageBox.Show("Dealerul nu are blackjack!");
+
+                }
+                
+        }
+
+        //reseteaza jocul
+        private void buttonrestart_Click(object sender, EventArgs e)
+        {
+            buttonhit.Visible = true;
+            buttonstand.Visible = true;
+            buttondoubledown.Visible = true;
+            buttonsurrender.Visible = true;
+            buttonrestart.Enabled = false;
+            buttondoubledown.Enabled = true;
+            buttonrestart.Text = "Alt joc";
+            ReseteazaJoc();
+        }
+
+
+        //daca apesi pe textul de "credite" 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if(credite<20)
+            {
+                if (credite == 1) MessageBox.Show("Ai 1 credit");
+                else MessageBox.Show("Ai " + credite + " credite");
+            }    
+            else MessageBox.Show("Ai " + credite + " de credite");
+        }
+
+        //devine int
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            miza = (int)numericUpDown1.Value;
+        }
+
+        //surrender. Cand jucatorul apasa acest buton, jocul se incheie si jucatorul primeste jumatate din miza.
+        private void buttonsurrender_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ai renuntat");
+            credite += (int)(miza / 2);
+            labelcredite.Text = "Credite: " + Convert.ToString(credite);
+            JocTerminat();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //butonul de hit
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hit();
+            buttondoubledown.Enabled = false;
+            VerificaPierdere();
+           
+        }
+        //dispar butoanele mai putin cel de start
+        private void JocTerminat()
+        {
+            buttonrestart.Enabled = true;
+            buttonhit.Visible = false;
+            buttonstand.Visible = false;
+            buttondoubledown.Visible = false;
+            buttonsurrender.Visible = false;
+        }
+        //verific daca am pierdut 
+        private void VerificaPierdere()
+        {
+            if (CartiTrase > 21)
+            {
+                MessageBox.Show("Ai pierdut");
+                JocTerminat();
+                if (credite <= 10)
+                {
+                    MessageBox.Show("Ai ramas fara credite. Jocul se va inchide.");
+                    Application.Exit();
+                }
+                labelcredite.Text = "Credite: " + Convert.ToString(credite);
+            }
+        }
+
+        //Jucatorul trage o carte
+        private void Hit()
+        {
+            
+            Random r = new Random();
+            Gasit = false;
+            while(Gasit==false)
+            {
+                int x = r.Next(0, 51);
+                if(carti[x]==0)
+                {
+                    carti[x] = 1;
+                    labeltest.Text += numecarti[x] + "\n";
+                    if (x % 13 == 0 && CartiTrase < 11)
+                    {
+                        CartiTrase += 11;
+                        ContineAs = true; 
+                    }
+                    else if (x % 13 + 1 > 10)
+                        CartiTrase += 10;
+                    else
+                        CartiTrase += x % 13 + 1;
+                    Gasit = true;
+                }
+            }
+            if (ContineAs == true && CartiTrase>21)
+            {
+                CartiTrase -= 10;
+                ContineAs = false;
+            }
+            labelscor.Text = Convert.ToString(CartiTrase);
+        }
+
+        //Dealerul trage o carte
+        private void HitDealer()
+        {
+            Random r = new Random();
+            Gasit = false;
+            while(Gasit==false)
+            {
+                int x = r.Next(0, 51);
+                if(carti[x]==0)
+                {
+                    carti[x] = 1;
+                    labeldealer.Text += numecarti[x]+ "\n";
+                    if (x % 13 == 0 && CartiDealer < 11)
+                    {
+                        CartiDealer += 11;
+                        ContineAsDealer = true;
+                    }
+                    else if (x % 13 + 1 > 10)
+                        CartiDealer += 10;
+                    else
+                        CartiDealer += x % 13 + 1;
+                    Gasit = true;
+                }
+            }
+            if(ContineAsDealer==true && CartiDealer>21)
+            {
+                CartiTrase -= 10;
+                ContineAsDealer= false;
+            }
+            labelscordealer.Text = Convert.ToString(CartiDealer);
+            
+        }
+    }
+}
